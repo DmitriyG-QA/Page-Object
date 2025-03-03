0149d8750b5f90fcb54fe9bbcc9759fca4cdc741
Dmitriy.G <dmitriy.g@breakout-group.com>
2025-03-03 16:50:02 +0300

    1

---
+++ birds/birds_1.py
@@ -0,0 +1,38 @@
+class Bird:
+    wings = True
+    beak = True
+    plumage = True
+
+    def fly(self):
+        print('Машу крыльями и лечу')
+
+    def walk(self):
+        pass
+
+
+class Sparrow(Bird):
+    size = 'small'
+    color = 'brown'
+
+    def walk(self):
+        print('прыг - прыг')
+
+class Duck(Bird):
+    def walk(self):
+        print('шагает')
+
+
+chizhik = Sparrow()
+pyzhik = Sparrow()
+utka = Duck()
+pyzhik.size = 'medium'
+
+print(chizhik.size)
+print(pyzhik.size)
+print(chizhik.color)
+print(pyzhik.color)
+chizhik.fly()
+utka.fly()
+pyzhik.walk()
+chizhik.walk()
+utka.walk()
